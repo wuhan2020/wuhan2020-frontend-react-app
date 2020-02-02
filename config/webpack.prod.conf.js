bf878ea5b625660303e9cb524001d0d24bb168bf
ZhexuanLiu <oozliuoo@gmail.com>
2020-02-02 11:25:02 +0800

    use relative path for css/js resources

--- config/webpack.prod.conf.js
+++ config/webpack.prod.conf.js
@@ -8,15 +8,15 @@ const webpackConfig = merge(baseConfig, {
 	mode: 'production',
 	output: {
 		path: path.resolve(__dirname, '..', 'wwwroot/dist'),
-		filename: 'http://q519ubblo.bkt.clouddn.com/static/js/app.bundle.[hash].js',
-		chunkFilename: 'http://q519ubblo.bkt.clouddn.com/static/js/[id].[chunkhash].js',
+		filename: './static/js/app.bundle.[hash].js',
+		chunkFilename: './static/js/[id].[chunkhash].js',
 		publicPath: process.env.PUBLIC_URL ? process.env.PUBLIC_URL : '',
 	},
 	plugins: [
 		...baseConfig.plugins,
 		new MiniCssExtractPlugin({
-			filename: 'http://q519ubblo.bkt.clouddn.com/static/css/[name].[hash].css',
-			chunkFilename: 'http://q519ubblo.bkt.clouddn.com/static/css/[id].[chunkhash].css',
+			filename: './static/css/[name].[hash].css',
+			chunkFilename: './static/css/[id].[chunkhash].css',
 		}),
 	],
 	optimization: {
@@ -28,7 +28,7 @@ const webpackConfig = merge(baseConfig, {
 					enforce: true,
 					test: /node_modules/,
 					name: 'vendor',
-					filename: 'http://q519ubblo.bkt.clouddn.com/static/js/[name].[hash].js',
+					filename: './static/js/[name].[hash].js',
 					priority: -10,
 				},
 			},
