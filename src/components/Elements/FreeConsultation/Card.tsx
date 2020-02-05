99e89a99a07359fcb7ce340562afcd1321130706
ZhexuanLiu <oozliuoo@gmail.com>
2020-02-05 15:57:54 +0800

    polish up consultation page

--- src/components/Elements/FreeConsultation/Card.tsx
+++ src/components/Elements/FreeConsultation/Card.tsx
@@ -3,6 +3,10 @@ import styles from '../../../styles/elements/free-consultation/card.module.scss'
 import Card from "../Card";
 import { IFreeConsultation } from "../../../types/interfaces";
 import Message from "../../../components/Message";
+import moment from "moment";
+import Button from "../Button";
+import { Icon } from "antd";
+import { isMobile } from "../../../utils/deviceHelper";
 
 interface FreeConsultationCardProps {
     data: IFreeConsultation;
@@ -15,18 +19,23 @@ export default class FreeConsultationCard extends React.PureComponent<FreeConsul
         const { data } = this.props;
     return (
       <Card className={styles.elementsFreeConsultationListCard}>
-          <div className="title">
-              {data.name}
-          </div>
-          <div className="date">
-            {data.date}
-          </div>
-          <div className="detail">
-            ""
-          </div>
-          <div className="info-phone">
-              <a className="info" href={data.url}>查看官方信息</a>
-              <div className="phone">{data.contacts.length > 0 ? data.contacts[0].tel : ""}</div>
+          <div className={styles.main}>
+            <section className={styles.generalInfo}>
+              <div className={styles.title}>{data.name}</div>
+              <div className={styles.date}>{moment(data.date).fromNow()}</div>
+              <div className={styles.detail}>{data.remark}</div>
+            </section>
+            <section className={styles.contactInfo}>
+                <div className={styles.infoItem}>
+                  <Icon type="desktop" />
+                  <Button type='link' href={data.url} target='_blank'>{Message('VIEW_OFFICIAL_INFO')}</Button>
+                </div>
+                {data.contacts[0] && data.contacts[0].tel ? <div className={styles.infoItem}>
+                  <Icon type="mobile" />
+                  <div className={styles.phone}>{data.contacts.length > 0 ? data.contacts[0].tel : ""}</div>
+                  {isMobile ? <Button type='link' href={`tel:${data.contacts[0].tel}`}>{Message('DIAL_PHONE')}</Button> : null}
+                </div> : null}
+            </section>
           </div>
       </Card>
     );
